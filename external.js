c62312f0c24069670913ef2f45146a7de49c118f
sBrunes <bruness@att.net>
2024-03-14 02:12:58 -0400

    up

---
+++ external.js
@@ -0,0 +1,26 @@
+var AllDecisionButtons = [];
+
+function aFunctionImplementedInExternalJsFile(jsInt) {
+	alert(jsInt + " from external js");
+}
+
+function AddNewButton(ButtonText)
+{
+	console.log("THis is adding a button");
+	alert("Adding a button with " + ButtonText);
+	const newButton = document.createElement('button');
+	newButton.textContent = ButtonText;
+	document.getElementById("ButtonParent").appendChild(newButton);
+
+	AllDecisionButtons.push(newButton);
+}
+
+function ClearAllDecisionButtons()
+{
+	for(let i = 0; i < AllDecisionButtons.length; i++)
+	{
+		AllDecisionButtons[i].parentNode.removeChild(AllDecisionButtons[i]);
+	}
+
+	AllDecisionButtons = [];
+}
